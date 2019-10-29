87b93218c898534d8f7e682c5031eb6211590f33
hasbi12muhammad <hasbi12.muhammad@gmail.com>
2019-10-29 22:20:32 +0700

    add function "mismatch_calc"

--- sub_loc_accuracy.py
+++ sub_loc_accuracy.py
@@ -23,7 +23,6 @@ sub_loc = ['A','B','C','D']
 data_sub_loc_ruang1 = [[],[],[],[]]
 data_sub_loc_ruang2 = [[],[],[],[]]
 data_sub_loc_lorong = [[],[],[],[]]
-data_true_loc = []
 
 item = data.values
 for i in range(len(data)):
@@ -37,26 +36,28 @@ for i in range(len(data)):
                 data_sub_loc_ruang2[j].append(item[i])
     else:
         for j in range(4):
-            if item[j][2] == sub_loc[j]:
+            if item[i][2] == sub_loc[j]:
                 data_sub_loc_lorong[j].append(item[i])
 # ignore all future warnings
 simplefilter(action='ignore', category=FutureWarning)
 
-# def mismatch_calc(arr):
-#     mismatch = o
-#     for i in range(len(arr)):
-#         for j in range(len(arr[i])):
-#             if arr[i]
-for i in range(len(data_sub_loc_ruang1)):
-    for j in range(len(data_sub_loc_ruang1[i])):
-        print(data_sub_loc_ruang1[i][j])
-print()
-for i in range(len(data_sub_loc_ruang2)):
-    for j in range(len(data_sub_loc_ruang2[i])):
-        print(data_sub_loc_ruang2[i][j])
-print()
-for i in range(len(data_sub_loc_lorong)):
-    for j in range(len(data_sub_loc_lorong[i])):
-        print(data_sub_loc_lorong[i][j])
+def mismatch_calc(arr):
+    mismatch = [0,0,0,0]
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            if arr[i][j][3] == "Mismatch":
+                mismatch[i] = mismatch[i] + 1
+    
+    for i in range(len(arr)):
+        percent = mismatch[i] / len(arr[i]) * 100
+        print("             Sub-lokasi",arr[i][0][2], "=",percent,"%")
+
+
+print("Kesalahan Prediksi pada Ruang 1 adalah :")
+mismatch_calc(data_sub_loc_ruang1)
+print("Kesalahan Prediksi pada Ruang 2 adalah :")
+mismatch_calc(data_sub_loc_ruang2)
+print("Kesalahan Prediksi pada Lorong adalah :")
+mismatch_calc(data_sub_loc_lorong)
 
 
